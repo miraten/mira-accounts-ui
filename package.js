54246550d585268f04d76ff54a3cf9d5248d6ff9
Lee Sangwon <lee@miraten.com>
2014-06-03 15:40:10 +0900

    Initial commit

---
+++ package.js
@@ -0,0 +1,38 @@
+Package.describe({
+  summary: "Accounts-ui widgets using Bootstrap 3.x"
+});
+
+Package.on_use(function (api) {
+  api.use(['deps', 'service-configuration', 'accounts-base',
+           'underscore', 'templating', 'session', 'mira-i18n'], 'client');
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+
+  // Allow us to call Accounts.oauth.serviceNames, if there are any OAuth
+  // services.
+  api.use('accounts-oauth', {weak: true});
+  // Allow us to directly test if accounts-password (which doesn't use
+  // Accounts.oauth.registerService) exists.
+  api.use('accounts-password', {weak: true});
+
+  api.add_files([
+    'accounts_ui.js',
+
+    'login_buttons.html',
+    'login_buttons_single.html',
+    'login_buttons_dropdown.html',
+    'login_buttons_dialogs.html',
+
+    'login_buttons_session.js',
+
+    'login_buttons.js',
+    'login_buttons_single.js',
+    'login_buttons_dropdown.js',
+    'login_buttons_dialogs.js'], 'client');
+});
+
+Package.on_test(function (api) {
+  api.use('mira-accounts-ui');
+  api.use('tinytest');
+  api.add_files('test/accounts_ui_tests.js', 'client');
+});
